04f7cf59663ac884fd2b61579f6b9fb891f1712a
ryanhaste <calvinhaste@gmail.com>
2022-06-24 15:00:02 +0300

    python basics

---
+++ basics.py
@@ -0,0 +1,14 @@
+def get_area (length = 0, width = 0):
+    area = length * width
+    print(area)
+
+def get_perimeter ( length = 0, width = 0):
+    perimeter = 2 * (length + width)
+    print(perimeter)
+
+get_area(20,10)
+get_perimeter(20,10) 
+
+get_area()
+get_perimeter()
+
